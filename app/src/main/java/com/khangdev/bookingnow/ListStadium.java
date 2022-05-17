e43de4e066d4ca8b272a79a785e0aec55df94336
Khangdev <volenhatkhang1808@gmail.com>
2022-05-17 23:52:24 +0700

    first commit

---
+++ app/src/main/java/com/khangdev/bookingnow/ListStadium.java
@@ -0,0 +1,14 @@
+package com.khangdev.bookingnow;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListStadium extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_stadium);
+    }
+}
